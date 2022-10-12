00a6c5bdeee7f11088ac4eecd56efd80b0ce522d
Emiliafc17 <108153621+Emiliafc17@users.noreply.github.com>
2022-10-12 01:57:41 -0500

    taller 3-4

---
+++ taller 3 - 4/Taller 3/form.js
@@ -0,0 +1,13 @@
+var n1= parseInt(prompt ("ingrese numero 1"));
+var n2=prompt("ingrese numero 2");
+var suma=n1+parseInt(n2);
+var resta= n1-n2;
+var multiplicacion=n1*n2;
+var division=n1/n2;
+let modulo= n1%n2;
+
+alert("La suma de los valores ingresados:"+suma);
+alert("la resta es: "+resta);
+alert("la multiplicacion es: "+multiplicacion);
+alert("la division es: "+division);
+alert("El resto es:"+modulo);
